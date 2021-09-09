e52d5514c07233c17cc16aa808b98cde9a27444d
Maliosman26 <muhammetaliosmanozyurt@gmail.com>
2021-09-09 12:31:06 +0300

    Controller Structure and Layout Arrangements

--- MvcProje/Controllers/HomeController.cs
+++ MvcProje/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MvcProje.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Senin hakkında Sayfan";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Senin İletişim Sayfan";
 
             return View();
         }
